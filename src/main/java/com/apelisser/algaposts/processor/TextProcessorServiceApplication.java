6b03fc8577c49d25f9872a8ed33469133e2ef8f7
Abner J Pelisser <abner.pelisser@gmail.com>
2025-06-21 17:35:39 -0300

    init: create modules and projects

---
+++ src/main/java/com/apelisser/algaposts/processor/TextProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.apelisser.algaposts.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextProcessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextProcessorServiceApplication.class, args);
+	}
+
+}
