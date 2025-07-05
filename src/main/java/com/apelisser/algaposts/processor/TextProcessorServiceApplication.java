b3b6a56e647278b028b87189890ff8e0f31ee5bf
Abner J Pelisser <abner.pelisser@gmail.com>
2025-07-04 23:51:15 -0300

    chore: configure app and rabbitmq

--- src/main/java/com/apelisser/algaposts/processor/TextProcessorServiceApplication.java
+++ src/main/java/com/apelisser/algaposts/processor/TextProcessorServiceApplication.java
@@ -3,10 +3,15 @@ package com.apelisser.algaposts.processor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
+import static java.time.ZoneOffset.UTC;
+
 @SpringBootApplication
 public class TextProcessorServiceApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone(UTC));
 		SpringApplication.run(TextProcessorServiceApplication.class, args);
 	}
 
---
+++ src/main/java/com/apelisser/algaposts/processor/core/rabbitmq/RabbitMQInitializer.java
@@ -0,0 +1,21 @@
+package com.apelisser.algaposts.processor.core.rabbitmq;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQInitializer {
+
+    private final RabbitAdmin rabbitAdmin;
+
+    public RabbitMQInitializer(RabbitAdmin rabbitAdmin) {
+        this.rabbitAdmin = rabbitAdmin;
+    }
+
+    @PostConstruct
+    public void initialize() {
+        rabbitAdmin.initialize();
+    }
+
+}
